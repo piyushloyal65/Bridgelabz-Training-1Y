e0c9a7784b87fd7a077cb3154867d2d0e61a5312
PIYUSH SHARMA <piyusharma0565@gmail.com>
2026-01-09 20:49:02 +0530

    first lecture

---
+++ Demo1.java
@@ -0,0 +1,19 @@
+public class Demo1{
+
+static int x = 5;;
+int y = 10;
+public static void main(String[] args){
+int p = 2;
+System.out.println("hello world!!");
+m1();
+
+}
+
+
+static void m1(){
+
+System.out.println("you are a foolish");
+System.out.println(x);
+}
+
+}
